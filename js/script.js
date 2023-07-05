45ccabc4909c1bfc8bb1538bc307362a3a53ef28
Samarth Shinde <samarthsshinde@gmail.com>
2023-07-05 12:16:29 +0530

    6. swiper slider

--- js/script.js
+++ js/script.js
@@ -173,11 +173,60 @@ function displayBackgroundImage(type, backgroundPath) {
     overlayDiv.style.opacity = '0.2';
 
 
-    if (type === 'show') {
-        document.querySelector('#show-details').appendChild(overlayDiv);
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
     } else {
         document.querySelector('#show-details').appendChild(overlayDiv);
     }
+}
+
+//Display Spinner
+async function displaySlider() {
+    const { results } = await fetchAPIData('movie/now_playing');
+
+    // console.log(results);
+
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} />
+            </a>
+            <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+        `;
+
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();
+    });
+
+}
+
+function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2
+            },
+            700: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            },
+        }
+    });
 }
 
 // Fetch data from TMDB API
@@ -225,6 +274,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
+            displaySlider();
             displayPopularMovies();
             break;
         case '/movie-details.html':
